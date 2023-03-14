7462c3be6a42d05f9f99639370a0ed0214371849
liyicheng215 <Yicheng3.22@intl.zju.edu.cn>
2023-03-14 09:23:44 +0100

    variable1

--- Practical5/Variable.py
+++ Practical5/Variable.py
@@ -5,3 +5,7 @@ d = a - b
 e = c - a
 print(d > e)
 # output is False, showing that e is larger and the distance going to Haining is longer
+X = c > a
+Y = c > a
+print(X and Y)  # output is True
+print(X or Y)  # output is False
