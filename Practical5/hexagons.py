2bfd628f55443d265ba3ba69b99c039bf8346930
liyicheng215 <Yicheng3.22@intl.zju.edu.cn>
2023-03-15 08:02:22 +0100

    hexagons1

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,5 +1,7 @@
 """
-for n= 1 2 3 4 5 we can use range to output n from 1 to 5 and use the print to print the outcome
+for n= 1 2 3 4 5  use range to output n from 1 to 5
+the equation of h and n
+print the outcome
 """
 for n in range(1 , 5) :
     h = n * (2 * n - 1)
